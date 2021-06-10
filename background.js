248fa98110e34e673eaad5e9bbb175d949bec569
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-06-10 21:17:08 +0200

    fix: only show context menu on active tab

--- background.js
+++ background.js
@@ -3,37 +3,51 @@ const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 const extdesc = manifest.description
 
-browser.menus.create({
-	id: extname,
-	title: extdesc,
-	contexts: ["tab"],
-	onclick: async function(info, tab) {
-		if(info.menuItemId.startsWith(extname)){
-			const url = new URL(tab.url);
-			const tabs = await browser.tabs.query({ 
-				url: url.origin + "/*", 
-				hidden: false
-			});
 
-			let tmp = {};
+browser.menus.onShown.addListener( async (info,tab) => {
+	if(tab.active) {
+		browser.menus.create({
+			id: extname,
+			title: extdesc,
+			contexts: ["tab"],
+			onclick: async function(info, tab) {
+				if(info.menuItemId.startsWith(extname)){
+					const url = new URL(tab.url);
+					const tabs = await browser.tabs.query({ 
+						url: url.origin + "/*", 
+						hidden: false
+					});
 
-			tmp[tab.windowId] = [tab.index];
+					let tmp = {};
 
-			tabs.forEach( (t) => {
-				if (typeof tmp[t.windowId] === 'undefined'){
-					tmp[t.windowId] = [];
-				} 
-				tmp[t.windowId].push(t.index);
-			});
+					tmp[tab.windowId] = [tab.index];
 
-			for (const [k,v] of Object.entries(tmp)) {
-				browser.tabs.highlight({
-					windowId: parseInt(k),
-					tabs: v,
-					populate: false
-				}); 
+					tabs.forEach( (t) => {
+						if (typeof tmp[t.windowId] === 'undefined'){
+							tmp[t.windowId] = [];
+						} 
+						tmp[t.windowId].push(t.index);
+					});
+
+					for (const [k,v] of Object.entries(tmp)) {
+						browser.tabs.highlight({
+							windowId: parseInt(k),
+							tabs: v,
+							populate: false
+						}); 
+					}
+				}
 			}
+		});
+	}else{
+		try {
+			await browser.menus.remove(extname);
+		}catch(e){
+			console.error(e);
 		}
 	}
+	browser.menus.refresh();
+
 });
+
 
